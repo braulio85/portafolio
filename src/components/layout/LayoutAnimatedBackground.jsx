3a7c5ee7c081fae18151dce7751c0c289c23f4ad
Braulio Echeverria <braulioecheverria@kinal.org.gt>
2026-01-07 19:12:15 -0600

    Fix: error utils no definido en LayoutAnimatedBackground

--- src/components/layout/LayoutAnimatedBackground.jsx
+++ src/components/layout/LayoutAnimatedBackground.jsx
@@ -15,6 +15,73 @@ function LayoutAnimatedBackground() {
     const visibilityClass =
         utils.device.isAndroid() && !utils.device.isChromeAndroid() ? `d-none` : ``
 
+    class CircleData {
+        constructor() {
+            this.reset()
+            this.didEnter = true
+        }
+
+        randomizeProps() {
+            const baseSize = Math.max(window.innerWidth, window.innerHeight)
+            this.radius = utils.number.random(baseSize / 24, baseSize / 8)
+            this.speedX = utils.number.random(3, 10, true)
+            this.speedY = utils.number.random(2, 5, true)
+            this.color = Math.random() > 0.5 ? 'dark' : 'light'
+            this.opacity = 0.1 + Math.random() * 0.9
+        }
+
+        update(dt) {
+            this.x += (this.speedX / 2) * dt
+            this.y += (this.speedY / 2) * dt
+
+            const outOfBounds = this.isOutOfBounds()
+            if (!this.didEnter) {
+                this.didEnter = outOfBounds
+            } else if (outOfBounds) {
+                this.reset()
+            }
+        }
+
+        isOutOfBounds() {
+            return (
+                this.x + this.radius * 2 < 0 ||
+                this.x - this.radius * 2 > window.innerWidth ||
+                this.y + this.radius * 2 < 0 ||
+                this.y - this.radius * 2 > window.innerHeight
+            )
+        }
+
+        reset() {
+            this.randomizeProps()
+
+            const direction = utils.number.random(0, 3)
+            switch (direction) {
+                case 0: // Left
+                    this.x = -this.radius * 2
+                    this.y = utils.number.random(0, window.innerHeight)
+                    this.speedX = Math.abs(this.speedX)
+                    break
+                case 1: // Right
+                    this.x = window.innerWidth + this.radius * 2
+                    this.y = utils.number.random(0, window.innerHeight)
+                    this.speedX = -1 * Math.abs(this.speedX)
+                    break
+                case 2: // Top
+                    this.x = utils.number.random(0, window.innerWidth)
+                    this.y = -this.radius * 2
+                    this.speedY = Math.abs(this.speedY)
+                    break
+                case 3: // Bottom
+                    this.x = utils.number.random(0, window.innerWidth)
+                    this.y = window.innerHeight + this.radius * 2
+                    this.speedY = -1 * Math.abs(this.speedY)
+                    break
+            }
+
+            this.didEnter = false
+        }
+    }
+
     /** @constructs **/
     useEffect(() => {
         const circles = Array.from(
@@ -77,73 +144,6 @@ function LayoutAnimatedBackground() {
             <canvas id={`layout-animated-background-canvas`} />
         </Animable>
     )
-}
-
-class CircleData {
-    constructor() {
-        this.reset()
-        this.didEnter = true
-    }
-
-    randomizeProps() {
-        const baseSize = Math.max(window.innerWidth, window.innerHeight)
-        this.radius = utils.number.random(baseSize / 24, baseSize / 8)
-        this.speedX = utils.number.random(3, 10, true)
-        this.speedY = utils.number.random(2, 5, true)
-        this.color = Math.random() > 0.5 ? 'dark' : 'light'
-        this.opacity = 0.1 + Math.random() * 0.9
-    }
-
-    update(dt) {
-        this.x += (this.speedX / 2) * dt
-        this.y += (this.speedY / 2) * dt
-
-        const outOfBounds = this.isOutOfBounds()
-        if (!this.didEnter) {
-            this.didEnter = outOfBounds
-        } else if (outOfBounds) {
-            this.reset()
-        }
-    }
-
-    isOutOfBounds() {
-        return (
-            this.x + this.radius * 2 < 0 ||
-            this.x - this.radius * 2 > window.innerWidth ||
-            this.y + this.radius * 2 < 0 ||
-            this.y - this.radius * 2 > window.innerHeight
-        )
-    }
-
-    reset() {
-        this.randomizeProps()
-
-        const direction = utils.number.random(0, 3)
-        switch (direction) {
-            case 0: // Left
-                this.x = -this.radius * 2
-                this.y = utils.number.random(0, window.innerHeight)
-                this.speedX = Math.abs(this.speedX)
-                break
-            case 1: // Right
-                this.x = window.innerWidth + this.radius * 2
-                this.y = utils.number.random(0, window.innerHeight)
-                this.speedX = -1 * Math.abs(this.speedX)
-                break
-            case 2: // Top
-                this.x = utils.number.random(0, window.innerWidth)
-                this.y = -this.radius * 2
-                this.speedY = Math.abs(this.speedY)
-                break
-            case 3: // Bottom
-                this.x = utils.number.random(0, window.innerWidth)
-                this.y = window.innerHeight + this.radius * 2
-                this.speedY = -1 * Math.abs(this.speedY)
-                break
-        }
-
-        this.didEnter = false
-    }
 }
 
 export default LayoutAnimatedBackground
