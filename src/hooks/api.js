e5b45d631d5277a2d8cc6411d2c7fc3f52204dce
Braulio Echeverria <braulioecheverria@kinal.org.gt>
2026-09-18 22:27:12 -0600

    fix: test del test del test 123

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -2,6 +2,8 @@ import emailjs from '@emailjs/browser'
 import { Constants } from '/src/hooks/constants.js'
 import { Utils } from '/src/hooks/utils.js'
 
+//guaenassssssssssssssssssssssss
+
 export const useApi = () => {
     return {
         validators,
